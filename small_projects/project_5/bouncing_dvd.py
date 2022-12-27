9940124b8ebea96949ab90aecf1f52000e24ffc7
Hideaki Fukami <denys.fukami@gmail.com>
2022-12-27 16:08:45 -0300

    Update bouncing_dvd.py

--- small_projects/project_5/bouncing_dvd.py
+++ small_projects/project_5/bouncing_dvd.py
@@ -74,3 +74,64 @@ def main():
                 logo[DIR] = UP_RIGHT
             elif logo[X] == 0 and logo[DIR] == DOWN_LEFT:
                 logo[DIR] = DOWN_RIGHT
+
+            # See if the logo bounces off the right edge:
+            # (WIDTH - 3 because 'DVD' has 3 letters.)
+            elif logo[X] == WIDTH - 3 and logo[DIR] == UP_RIGHT:
+                logo[DIR] = UP_LEFT
+            elif logo[X] == WIDTH - 3 and logo[DIR] == DOWN_RIGHT:
+                logo[DIR] = DOWN_LEFT
+
+            # See if the logo bounces off the top edge:
+            elif logo[Y] == 0 and logo[DIR] == UP_LEFT:
+                logo[DIR] = DOWN_LEFT
+            elif logo[Y] == 0 and logo[DIR] == UP_RIGHT:
+                logo[DIR] = DOWN_RIGHT
+
+            # See if the logo bounces off the bottom edge:
+            elif logo[Y] == HEIGHT - 1 and logo[DIR] == DOWN_LEFT:
+                logo[DIR] = UP_LEFT
+            elif logo[Y] == HEIGHT - 1 and logo[DIR] == DOWN_RIGHT:
+                logo[DIR] = UP_RIGHT
+
+            if logo[DIR] != originalDirection:
+                # Change the color when the logo bounces:
+                logo[COLOR] = random.choice(COLORS)
+            
+            # Move the logo. (X moves 2 because the terminal characters are twice as tall as they are wide.)
+            if logo[DIR] == UP_RIGHT:
+                logo[X] += 2
+                logo[Y] -= 1
+            elif logo[DIR] == UP_LEFT:
+                logo[X] -= 2
+                logo[Y] -= 1
+            elif logo[DIR] == DOWN_RIGHT:
+                logo[X] += 2
+                logo[Y] += 1
+            elif logo[DIR] == DOWN_LEFT:
+                logo[X] -= 2
+                logo[Y] += 1
+        
+        # Display number of corner bounces:
+        bext.goto(5, 0)
+        bext.fg('white')
+        print('Corner bounces:', cornerBounces, end='')
+
+        for logo in logos:
+            # Draw the logos at their new location:
+            bext.goto(logo[X], logo[Y])
+            bext.fg(logo[COLOR])
+            print('DVD', end='')
+        
+        bext.goto(0, 0)
+
+        sys.stdout.flush() # Require for bext-using programs.
+        time.sleep(PAUSE_AMOUNT)
+
+    if __name__ == '__main__':
+        try:
+            main()
+        except KeyboardInterrupt:
+            print()
+            print('Bouncing DVD Logo, by Hideaki Fukami.')
+            sys.exit()
