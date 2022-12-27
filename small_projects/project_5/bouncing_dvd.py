b2712da71667c902b50aea0c739542d3ae83cada
Hideaki Fukami <denys.fukami@gmail.com>
2022-12-27 16:26:56 -0300

    Update bouncing_dvd.py

--- small_projects/project_5/bouncing_dvd.py
+++ small_projects/project_5/bouncing_dvd.py
@@ -1,18 +1,18 @@
 """Bouncing DVD Logo, by Hideaki Fukami
 A boucing DVD logo animation. You have to be "of a certain age" to appreciate this. Press Ctrl-C to stop.
 
-NOTE: Do not resize the terminal window while this program is running."""
+NOT: Do not resize the terminal window while this program is running."""
 
 import sys, random, time
 
 try:
     import bext
-except imporError:
+except ImportError:
     print('This program requires the bext module, which you\ncan install by following the instructions at\nhttps://pypi.org/project/Bext/')
     sys.exit()
 
 # Set up the constants:
-WIDTH, HEIGH = bext.size()
+WIDTH, HEIGHT = bext.size()
 
 # We can't print to the last column on Window without it adding a newline automatically, so reduce the width by one:
 WIDTH -= 1
@@ -41,7 +41,7 @@ def main():
     for i in range(NUMBER_OF_LOGOS):
         logos.append({COLOR: random.choice(COLORS),
                     X: random.randint(1, WIDTH - 4),
-                    Y: random.randint(1, HEIGH - 4),
+                    Y: random.randint(1, HEIGHT - 4),
                     DIR: random.choice(DIRECTIONS)})
         if logos[-1][X] % 2 == 1:
             # X is even so it can hit the corner
@@ -59,13 +59,13 @@ def main():
             if logo[X] == 0 and logo[Y] == 0:
                 logo[DIR] = DOWN_RIGHT
                 cornerBounces += 1
-            elif logo[X] == 0 and logo[Y] == HEIGH-1:
+            elif logo[X] == 0 and logo[Y] == HEIGHT-1:
                 logo[DIR] = UP_RIGHT
                 cornerBounces += 1
             elif logo[X] == WIDTH - 3 and logo[Y] == 0:
                 logo[DIR] = DOWN_LEFT
                 cornerBounces += 1
-            elif logo[X] == WIDTH - 3 and logo[Y] == HEIGH - 1:
+            elif logo[X] == WIDTH - 3 and logo[Y] == HEIGHT - 1:
                 logo[DIR] = UP_LEFT
                 cornerBounces += 1
             
@@ -128,10 +128,10 @@ def main():
         sys.stdout.flush() # Require for bext-using programs.
         time.sleep(PAUSE_AMOUNT)
 
-    if __name__ == '__main__':
-        try:
-            main()
-        except KeyboardInterrupt:
-            print()
-            print('Bouncing DVD Logo, by Hideaki Fukami.')
-            sys.exit()
+if __name__ == '__main__':
+    try:
+        main()
+    except KeyboardInterrupt:
+        print()
+        print('Bouncing DVD Logo, by Hideaki Fukami.')
+        sys.exit()
