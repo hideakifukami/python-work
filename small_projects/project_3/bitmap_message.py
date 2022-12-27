894e03a642679ab95ccfbe5c1a613206ddbcdd2e
Hideaki Fukami <denys.fukami@gmail.com>
2022-12-26 22:09:28 -0300

    Update bitmap_message.py

--- small_projects/project_3/bitmap_message.py
+++ small_projects/project_3/bitmap_message.py
@@ -3,8 +3,6 @@ Display a text message according to the provided bitmap image."""
 
 import sys
 
-
-
 bitmap = '''
                                                **
                                               ****
@@ -33,3 +31,21 @@ bitmap = '''
  *     ***        *******
 *        ***        **
 *************'''
+
+print('''Bitmap Message, by Hideaki Fukami
+Enter the message to display with the bitmap.''')
+message = input('> ')
+if message == '':
+    sys.exit()
+
+# Loop over each line in the bitmap:
+for line in bitmap.splitlines():
+    # Loop over each character in the line:
+    for i, bit in enumerate(line):
+        if bit == ' ':
+            print(' ', end='')
+        else:
+            print(message[i % len(message)], end='')
+    print()
+
+print()
